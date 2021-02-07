33ea3b3fe2820c06b5b40945a5bda8507ed53761
javi <jsmart1993@hotmail.com>
2021-02-06 17:57:44 -0700

    created animal interface

--- src/app/animals/animal-list/animal-list.component.ts
+++ src/app/animals/animal-list/animal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../animal.model'
 
 @Component({
   selector: 'app-animal-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimalListComponent implements OnInit {
 
+  animals:Animal[] = [
+    new Animal('Panthera leo', [], 'Panthera', 'Pantherinae', 'Felidae', 'Feliformia', 'Carnivora', 'Mammalia', 'Chordata', 'Lion', 'Vulnerable', ['https://unsplash.com/photos/IPRFX7CVVoU'], 'Cheeks')
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/animals/animal.model.ts
+++ src/app/animals/animal.model.ts
@@ -1,24 +1,32 @@
 export class Animal {
     public species: string;
+    public subspecies: [];
     public genus: string;
     public subfamily: string;
     public family: string;
+    public suborder: string;
     public order: string;
+    public animalClass: string;
     public phylum: string;
     public name: string;
     public conservationStatus: string;
-    public images: [];
+    public images: string[];
+    public description: string;
 
-    constructor(species: string, genus: string, subfamily: string, family: string, order: string, phylum: string, name: string, conservationStatus: string, images: []){
+    constructor(species: string, subspecies: [], genus: string, subfamily: string, family: string, suborder: string, order: string, animalClass: string, phylum: string, name: string, conservationStatus: string, images: string[], description: string){
         this.species = species;
+        this.subspecies = [];
         this.genus = genus;
         this.subfamily = subfamily;
         this.family = family;
         this.order = order;
+        this.suborder = suborder;
         this.phylum = phylum;
+        this.animalClass = animalClass;
         this.name = name;
         this.conservationStatus = conservationStatus;
         this.images = images;
+        this.description = description;
     }
 
 }
